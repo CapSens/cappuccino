c4df3b2ca0cf2fed2f6e3c58cd8c746286a18fc0
Yassine Zenati <yassine@capsens.eu>
2016-05-11 13:31:21 +0200

    Added move, copy, delete types

--- main.go
+++ main.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "os"
-  "fmt"
-  "log"
-  "bytes"
-  "regexp"
-  "strings"
-  "os/exec"
-  "io/ioutil"
-  "gopkg.in/yaml.v2"
-  "github.com/fatih/color"
-  "github.com/jessevdk/go-flags"
+	"bytes"
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/jessevdk/go-flags"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 const VERSION = "0.0.1"
 
 type GitOptions struct {
-  GitUrl string `short:"g" long:"git" description:"Git repository to clone"`
-  Branch string `short:"b" long:"branch" description:"Branch to work with" default:"master"`
+	GitUrl string `short:"g" long:"git" description:"Git repository to clone"`
+	Branch string `short:"b" long:"branch" description:"Branch to work with" default:"master"`
 }
 
 type Options struct {
-  Git     GitOptions
-  Debug   bool `short:"d" long:"debug" description:"Run in debug mode"`
-  Version bool `short:"v" long:"version" description:"Display program version"`
+	Git     GitOptions
+	Debug   bool `short:"d" long:"debug" description:"Run in debug mode"`
+	Version bool `short:"v" long:"version" description:"Display program version"`
 }
 
 /*
@@ -33,9 +33,9 @@ type Options struct {
   Consists of an engine name, associated version and an array of actions.
 */
 type Config struct {
-  Engine  string
-  Version string
-  Actions []Action
+	Engine  string
+	Version string
+	Actions []Action
 }
 
 /*
@@ -44,41 +44,51 @@ type Config struct {
   Consists of a name and an array of action commands.
 */
 type Action struct {
-  Name     string
-  Commands []ActionCommand
+	Name    string
+	Content []ActionContent
 }
 
 /*
-  ActionCommand
+  ActionContent
   Structure mirroring the format of a valid command if a config file.
   Consists of a type and a string command.
 */
-type ActionCommand struct {
-  Type    string
-  Command string
+type ActionContent struct {
+	Type        string
+	Path        string
+	Command     string
+	Source      string
+	Destination string
+	Arguments   ActionCommandArgument
+}
+
+type ActionCommandArgument struct {
+	Variable string
+	Path     string
+	Value    string
 }
 
 func main() {
-  var opts Options
-  var parser = flags.NewParser(&opts, flags.Default)
+	var opts Options
+	var parser = flags.NewParser(&opts, flags.Default)
 
-  if _, err := parser.Parse(); err != nil {
-    os.Exit(1)
-  }
+	if _, err := parser.Parse(); err != nil {
+		os.Exit(1)
+	}
 
-  if opts.Version {
-    fmt.Printf("%s %s\n", os.Args[0], VERSION)
-  }
+	if opts.Version {
+		fmt.Printf("%s %s\n", os.Args[0], VERSION)
+	}
 
-  gitUrl := opts.Git.GitUrl
-  gitBranch := opts.Git.Branch
+	gitUrl := opts.Git.GitUrl
+	gitBranch := opts.Git.Branch
 
-  if gitUrl != "" {
-    startEngine()
-    verifyGitUrl(gitUrl)
-    cloneRepo(gitUrl, gitBranch)
-    unmarshalConfig(gitUrl)
-  }
+	if gitUrl != "" {
+		startEngine()
+		verifyGitUrl(gitUrl)
+		cloneRepo(gitUrl, gitBranch)
+		unmarshalConfig(gitUrl)
+	}
 }
 
 /*
@@ -88,16 +98,18 @@ func main() {
   Same logic should be applied for a SVN cloning process.
 */
 func verifyGitUrl(href string) {
-  text(fmt.Sprintf("Checking git url format (%s)", href), color.FgYellow)
-  regex := "((git|ssh|http(s)?)|(git@[\\w\\.]+))(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)(/)?"
+	text(fmt.Sprintf("Checking git url format (%s)", href), color.FgYellow)
+	regex := "((git|ssh|http(s)?)|(git@[\\w\\.]+))(:(//)?)([\\w\\.@\\:/\\-~]+)(\\.git)(/)?"
 
-  match, _ := regexp.MatchString(regex, href)
-  if match {
-    text(fmt.Sprintf("Git url format successfuly verified"), color.FgYellow)
-  } else {
-    text(fmt.Sprintf("Git url format is not valid"), color.FgRed)
-    os.Exit(0)
-  }
+	match, _ := regexp.MatchString(regex, href)
+	if match {
+		content := fmt.Sprintf("Git url format successfuly verified")
+		text(content, color.FgYellow)
+	} else {
+		content := fmt.Sprintf("Git url format is not valid")
+		text(content, color.FgRed)
+		os.Exit(0)
+	}
 }
 
 /*
@@ -106,8 +118,9 @@ func verifyGitUrl(href string) {
   Same logic could be applied for an SVN repo.
 */
 func cloneRepo(href string, branch string) {
-  text(fmt.Sprintf("Cloning git repository (branch: %s)", branch), color.FgYellow)
-  executeCommand("git", "clone", href, "-b", branch)
+	content := fmt.Sprintf("Cloning git repository (branch: %s)", branch)
+	text(content, color.FgYellow)
+	executeCommand("git", "clone", href, "-b", branch)
 }
 
 /*
@@ -117,50 +130,114 @@ func cloneRepo(href string, branch string) {
   and falls through the execution process.
 */
 func unmarshalConfig(href string) {
-  config := Config{}
-  repoName := "app"
-  os.Chdir(repoName)
-  content, err := ioutil.ReadFile(".cappuccino.yml")
+	config := Config{}
+	repoName := "app"
+	os.Chdir(repoName)
+	content, err := ioutil.ReadFile(".cappuccino.yml")
 
-  if err != nil {
-    text("Error opening .cappuccino.yml file", color.FgRed)
-    os.Exit(0)
-  }
+	if err != nil {
+		text("Error opening .cappuccino.yml file", color.FgRed)
+		os.Exit(0)
+	}
 
-  text("File .cappuccino.yml detected", color.FgYellow)
+	text("File .cappuccino.yml detected", color.FgYellow)
 
-  err = yaml.Unmarshal(content, &config)
-  if err != nil {
-    log.Fatalf("Error: %v", err)
-  }
+	if err := yaml.Unmarshal(content, &config); err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 
-  displayVersion(&config)
-  parseConfig(&config)
+	displayVersion(&config)
+	processConfig(&config)
 }
 
 /*
-  parseConfig
+  processConfig
   Takes a Config pointer in argument and loops through the list
   of actions and commands, executing one after another in a
   thread safe executeCommand function.
 */
-func parseConfig(config *Config) {
-  text("Starting actions execution", color.FgYellow)
-  removeGitDirectory()
+func processConfig(config *Config) {
+	text("Starting actions execution", color.FgYellow)
+	removeGitDirectory()
 
-  for i := 0; i < len(config.Actions); i++ {
-    action := config.Actions[i]
-    text(action.Name, color.FgGreen)
+	for i := 0; i < len(config.Actions); i++ {
+		processAction(&config.Actions[i])
+	}
+}
 
-    for j := 0; j < len(action.Commands); j++ {
-      command := action.Commands[j]
-      content := fmt.Sprintf("\t-> %s", command.Command)
-      text(content, color.FgGreen)
+func processAction(action *Action) {
+	text(action.Name, color.FgGreen)
 
-      executableCommand := strings.Split(command.Command, " ")
-      executeCommand(executableCommand[0], executableCommand[1:]...)
-    }
-  }
+	for j := 0; j < len(action.Content); j++ {
+		processContent(&action.Content[j])
+	}
+}
+
+func processContent(content *ActionContent) {
+	if content.Type == "exec" {
+		command := content.Command
+		coloredContent := fmt.Sprintf("\t-> %s", command)
+		text(coloredContent, color.FgGreen)
+
+		executableCommand := strings.Split(command, " ")
+		executeCommand(executableCommand[0], executableCommand[1:]...)
+	}
+
+	if content.Type == "replace" {
+		variable := content.Arguments.Variable
+		value := content.Arguments.Value
+		path := content.Arguments.Path
+
+		read, err := ioutil.ReadFile(path)
+		if err != nil {
+			text(err.Error(), color.FgRed)
+			os.Exit(0)
+		}
+
+		varName := fmt.Sprintf("[cappuccino-var-%s]", variable)
+		newContent := strings.Replace(string(read), varName, value, -1)
+
+		if err := ioutil.WriteFile(path, []byte(newContent), 0); err != nil {
+			text(err.Error(), color.FgRed)
+			os.Exit(0)
+		}
+
+		coloredName := colored(variable, color.FgCyan)
+		coloredContent := fmt.Sprintf("\t-> %s", coloredName)
+		text(coloredContent, color.FgGreen)
+	}
+
+	if content.Type == "copy" {
+		source := content.Source
+		destination := content.Destination
+
+		coloredSource := colored(source, color.FgMagenta)
+		coloredContent := fmt.Sprintf("\t-> %s", coloredSource)
+		text(coloredContent, color.FgGreen)
+
+		executeCommand("cp", source, destination)
+	}
+
+	if content.Type == "move" {
+		source := content.Source
+		destination := content.Destination
+
+		coloredSource := colored(source, color.FgMagenta)
+		coloredContent := fmt.Sprintf("\t-> %s", coloredSource)
+		text(coloredContent, color.FgGreen)
+
+		executeCommand("mv", source, destination)
+	}
+
+	if content.Type == "delete" {
+		path := content.Path
+
+		coloredSource := colored(path, color.FgMagenta)
+		coloredContent := fmt.Sprintf("\t-> %s", coloredSource)
+		text(coloredContent, color.FgGreen)
+
+		executeCommand("rm", path)
+	}
 }
 
 /*
@@ -170,29 +247,27 @@ func parseConfig(config *Config) {
   stdout in case the debug mode is enabled, and omit otherwize.
 */
 func executeCommand(command string, args ...string) {
-  cmd := exec.Command(command, args...)
+	cmd := exec.Command(command, args...)
 
-  var out bytes.Buffer
-  var stderr bytes.Buffer
+	var out bytes.Buffer
+	var stderr bytes.Buffer
 
-  cmd.Stdout = &out
-  cmd.Stderr = &stderr
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
 
-  err := cmd.Run()
+	if err := cmd.Run(); err != nil {
+		text(stderr.String(), color.FgRed)
+		os.Exit(0)
+	}
 
-  if err != nil {
-    text(stderr.String(), color.FgRed)
-    os.Exit(0)
-  }
-
-  if out.String() != "" && false {
-    text(out.String(), color.FgGreen)
-  }
+	if out.String() != "" && false {
+		text(out.String(), color.FgGreen)
+	}
 }
 
 func removeGitDirectory() {
-  text("Removing existing .git folder", color.FgGreen)
-  executeCommand("rm", "-rf", ".git")
+	text("Removing existing .git folder", color.FgGreen)
+	executeCommand("rm", "-rf", ".git")
 }
 
 /*
@@ -200,7 +275,7 @@ func removeGitDirectory() {
   Displays a welcome message and current version once libraries are ready.
 */
 func startEngine() {
-  text(fmt.Sprintf("Starting engine (%s)", VERSION), color.FgYellow)
+	text(fmt.Sprintf("Starting engine (%s)", VERSION), color.FgYellow)
 }
 
 /*
@@ -209,7 +284,7 @@ func startEngine() {
   Please refer to the CHANGELOG for related changes.
 */
 func displayVersion(config *Config) {
-  text("Detected version: " + config.Version, color.FgYellow)
+	text("Detected version: "+config.Version, color.FgYellow)
 }
 
 /*
@@ -217,7 +292,7 @@ func displayVersion(config *Config) {
   Displays a prefix to all engine related messages
 */
 func prefix() string {
-  return fmt.Sprintf(strings.ToUpper("engine"))
+	return fmt.Sprintf(strings.ToUpper("engine"))
 }
 
 /*
@@ -225,6 +300,9 @@ func prefix() string {
   Displays a message on the screen using a particular color
 */
 func text(content string, attribute color.Attribute) {
-  _color := color.New(attribute).SprintFunc()
-  fmt.Printf("%s %s\n", _color(prefix()), content)
+	fmt.Printf("%s %s\n", colored(prefix(), attribute), content)
+}
+
+func colored(text string, attribute color.Attribute) string {
+	return color.New(attribute).SprintFunc()(text)
 }
